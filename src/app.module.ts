ac3a9b2059c19074033319488766c658eb1fed44
jFroylan-Cal <j.froylan_calderon@outlook.com>
2023-01-27 13:51:10 -0600

    Ädd Joi configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { SeedModule } from './seed/seed.module';
 import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
     }),
     PokemonModule,
     MongooseModule.forRoot(process.env.MONGODB),
